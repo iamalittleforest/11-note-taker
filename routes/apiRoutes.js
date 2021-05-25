30ae4736f60b05248790a38aeea7f51af13160cb
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-24 21:31:49 -0700

    refactor functions and add comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,17 @@
 const fs = require('fs');
 
 // export api routes
-module.exports = function(app) {
+module.exports = (app) => {
 
   // reads db.json and parses into JSON
-  const allNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-  
+  const allNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+
+  // return notes as JSON
   app.get('/api/notes', (req, res) => {
     return res.json(allNotes);
   });
   
-  app.post('/api/notes', (req, res) + {
+  // app.post('/api/notes', (req, res) + {
     
-  })
+  // })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,15 @@
 const path = require('path');
 
 // export html routes
-module.exports = function(app) {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+module.exports = (app) => {
   
+  // return notes.html file 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // return index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 }
