f6c48598cbd8b09599028f3632842799d00dd223
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-25 18:22:42 -0700

    add uuid for unique numbering

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,23 @@
 // dependencies
 const fs = require('fs');
+const uuid = require('uuid').v4;
 
 // export api routes
 module.exports = (app) => {
 
   // reads db.json and parses into JSON
-  const allNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+  let allNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
   // return notes as JSON
   app.get('/api/notes', (req, res) => {
     return res.json(allNotes);
   });
   
-  // app.post('/api/notes', (req, res) + {
-    
-  // })
+  // receive new note to save on the request body
+  app.post('/api/notes', (req, res) => {
+    const newNote = {...req.body, id: uuid()};
+
+    console.log(newNote);
+    res.json(newNote);
+  })
 }
